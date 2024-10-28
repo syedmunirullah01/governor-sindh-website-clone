b68e1c25fe50a7affd1eb4dd51b2957cfbf128d4
Syed Munir Ullah <syedmunirullah01@gmail.com>
2024-10-29 01:20:04 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import About from "@/components/About";
 import Courses from "@/components/Courses";
-import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import ImageSlider from "@/components/ImageSlider";
 
